38fe1a3fb0398d38e22218ad40cb995bb07f409a
NanairoMegane <kobarengaruda@gmail.com>
2019-03-04 16:44:28 +0900

    first commit

---
+++ src/webserver.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	// ハンドラーを貼るパスの指定です。今回は他のディレクトリもないので、ルートを指定します。
+	rootPath := "/"
+	// 返却するファイルが配置されている場所のパスです。
+	docDir := "/Users/kren/Documents/11 dataChase/Go/0303_WebServerTest/doc"
+
+	http.HandleFunc(
+		rootPath,
+		func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, docDir)
+		},
+	)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
